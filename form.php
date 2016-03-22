a869c34ab7ac5511eb460fd69d1ff3dd242a9fa1
r0ma <wbfx@ya.ru>
2016-03-22 23:38:52 +0500

    Первый коммит

---
+++ form.php
@@ -0,0 +1,38 @@
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<!-- <link href="/css/bootstrap.min.css" rel="stylesheet"> -->
+
+
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<!-- <script src="/js/jquery-1.2.1.min.js"></script> -->
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<!-- <script src="/js/bootstrap.min.js"></script> -->
+
+
+
+<div class="main">
+
+	<form action="" method="POST">
+	
+	<p>
+	  <input type="text" name="name" id="name" value="" placeholder="Имя">
+	</p>
+
+	<p>
+	  <input type="text" name="email" id="email" value="" placeholder="e-mail">
+	</p>
+	
+	<p>
+	  <input type="text" name="subject" id="subject" value="" placeholder="Тема">
+	</p>
+
+	<p>
+	  <textarea placeholder="текст сообщения"></textarea>
+	</p>
+
+	<input type="submit" name="submit" id="submit" value="Отправить">
+	
+	</form>
+	
+</div>
